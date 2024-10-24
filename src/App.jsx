a8054df2fbdd7d79bddb4c9acee6549c17bf0f05
Nahid Hasan <nahidn228@gmail.com>
2024-10-24 19:24:46 +0600

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Dream XI</h1>
+      <Header></Header>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { TbCoin } from "react-icons/tb";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+
+              <li>
+                <a>Fixture</a>
+              </li>
+              <li>
+                <a>Teams</a>
+              </li>
+              <li>
+                <a>Schedules</a>
+              </li>
+            </ul>
+          </div>
+
+          <img src={"https://i.ibb.co.com/qkxHQdB/logo.png"} alt="" />
+        </div>
+        
+        <div className="navbar-end gap-2">
+        <ul className="menu menu-horizontal px-1 hidden lg:flex font-semibold">
+            <li>
+              <a>Home</a>
+            </li>
+
+            <li>
+              <a>Fixture</a>
+            </li>
+            <li>
+              <a>Teams</a>
+            </li>
+            <li>
+              <a>Schedules</a>
+            </li>
+          </ul>
+          <a className="btn text-lg font-semibold">
+            0 Coin{" "}
+            <span className="text-[#F39E09]">
+              <TbCoin />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
