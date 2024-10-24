868b092421a3fe2cc9926a2a1f511e6a2b152c3a
Nahid Hasan <nahidn228@gmail.com>
2024-10-24 18:57:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center">Dream XI</h1>
+    </>
+  );
+}
+
+export default App;
